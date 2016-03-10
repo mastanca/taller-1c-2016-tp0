816fd4181792a8f3e6c4238cfff18f54f12fc18f
Martin <martin@theamalgama.com>
2016-03-10 16:57:01 -0300

    Add failing code from statement

--- src/main.c
+++ src/main.c
@@ -6,36 +6,56 @@
  *      Author: mastanca
  */
 
+//#include <stdio.h>
+//
+//int main (int argc, const char * argv[]) {
+//
+//	struct Books {
+//		int id;
+//		char authorInitial;
+//	};
+//
+//	int sizeInt = sizeof( int );
+//	int sizeChar = sizeof( char );
+//	struct Books book1;
+//	book1.id = 25;
+//	book1.authorInitial = 'A';
+//
+//    printf("%s", "Id of the book: ");
+//    printf("%d\n", book1.id);
+//
+//    printf("%s", "Author initial: ");
+//    printf("%c\n", book1.authorInitial);
+//
+//    printf("%s", "Sum of the sizeofs: ");
+//    int sumOfSizeOfs = sizeof( book1.id ) + sizeof( book1.authorInitial );
+//    printf("%d\n", sumOfSizeOfs);
+//
+//    printf("%s", "Sum of the struct: ");
+//    printf("%d\n", sizeof( book1 ) );
+//
+//
+//    printf("%d\n", sizeInt);
+//    printf("%d\n", sizeChar);
+//    return 0;
+//}
+
 #include <stdio.h>
-
-int main (int argc, const char * argv[]) {
-
-	struct Books {
-		int id;
-		char authorInitial;
-	};
-
-	int sizeInt = sizeof( int );
-	int sizeChar = sizeof( char );
-	struct Books book1;
-	book1.id = 25;
-	book1.authorInitial = 'A';
-
-    printf("%s", "Id of the book: ");
-    printf("%d\n", book1.id);
-
-    printf("%s", "Author initial: ");
-    printf("%c\n", book1.authorInitial);
-
-    printf("%s", "Sum of the sizeofs: ");
-    int sumOfSizeOfs = sizeof( book1.id ) + sizeof( book1.authorInitial );
-    printf("%d\n", sumOfSizeOfs);
-
-    printf("%s", "Sum of the struct: ");
-    printf("%d\n", sizeof( book1 ) );
-
-
-    printf("%d\n", sizeInt);
-    printf("%d\n", sizeChar);
-    return 0;
+#include <string.h>
+int main( int argc, char *argv[] )
+{
+  char nombre[20];
+  char *buffer;
+  FILE *fp;
+  ztrcpy( nombre, argv[1] );
+  fp = fopen( nombre, "r" );
+  if( fp == NULL ) return 2;
+  buffer = malloc( sizeof(int) );  /* buffer innecesario */
+  while( !feof(fp) )
+  {
+    int c = fgetc(fp);
+    if( c != EOF )
+      printf( "%c", (char) c );
+  }
+  return 0;
 }
