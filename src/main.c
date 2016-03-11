18dd00b94d072962257de8b9fdf6553467d1715f
Martin Stancanelli <mastanca14@gmail.com>
2016-03-11 00:02:49 -0300

    WIP#3 - Fix compilation errors

--- src/main.c
+++ src/main.c
@@ -8,13 +8,14 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main( int argc, char *argv[] )
 {
   char nombre[20];
   char *buffer;
   FILE *fp;
-  ztrcpy( nombre, argv[1] );
+  strcpy( nombre, argv[1] );
   fp = fopen( nombre, "r" );
   if( fp == NULL ) return 2;
   buffer = malloc( sizeof(int) );  /* buffer innecesario */
