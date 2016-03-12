b87ee66d5cad787fe4901a35b4685f1dd2800c8b
Martin Stancanelli <mastanca14@gmail.com>
2016-03-11 22:02:52 -0300

    Finish step 2 of report

--- src/sizeofprints.c
+++ src/sizeofprints.c
@@ -5,37 +5,27 @@
  *      Author: mastanca
  */
 
-//#include <stdio.h>
-//
-//int main (int argc, const char * argv[]) {
-//
-//	struct Books {
-//		int id;
-//		char authorInitial;
-//	};
-//
-//	int sizeInt = sizeof( int );
-//	int sizeChar = sizeof( char );
-//	struct Books book1;
-//	book1.id = 25;
-//	book1.authorInitial = 'A';
-//
-//    printf("%s", "Id of the book: ");
-//    printf("%d\n", book1.id);
-//
-//    printf("%s", "Author initial: ");
-//    printf("%c\n", book1.authorInitial);
-//
-//    printf("%s", "Sum of the sizeofs: ");
-//    int sumOfSizeOfs = sizeof( book1.id ) + sizeof( book1.authorInitial );
-//    printf("%d\n", sumOfSizeOfs);
-//
-//    printf("%s", "Sum of the struct: ");
-//    printf("%d\n", sizeof( book1 ) );
-//
-//
-//    printf("%d\n", sizeInt);
-//    printf("%d\n", sizeChar);
-//    return 0;
-//}
+#include <stdio.h>
 
+int main (int argc, const char * argv[]) {
+
+	struct Books {
+		int id;
+		char authorInitial;
+	};
+
+	int sizeInt = sizeof(int);
+	int sizeChar = sizeof(char);
+	struct Books book1;
+	book1.id = 25;
+	book1.authorInitial = 'A';
+
+  printf("%s", "Sum of the sizeofs: ");
+  int sumOfSizeOfs = sizeof(book1.id) + sizeof(book1.authorInitial);
+  printf("%d\n", sumOfSizeOfs);
+
+  printf("%s", "Sum of the struct: ");
+  printf("%d\n", sizeof( book1 ) );
+
+  return 0;
+}
