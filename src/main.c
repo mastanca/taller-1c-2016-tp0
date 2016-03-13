71696b83734407fbe80a793c55e6f1a9e74c8073
Martin Stancanelli <mastanca14@gmail.com>
2016-03-13 15:45:44 -0300

    Change answer of erros to compiler error

--- src/main.c
+++ src/main.c
@@ -8,26 +8,26 @@
 
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
+// #include <stdlib.h>
 #define LARGO_FILE 20
 #define POS_NOMBRE_ARCHIVO 1
 #define ARCHIVO_NO_ENCONTRADO 1
 #define SALIDA_NORMAL 0
 int main(int argc, char *argv[])
 {
-  FILE *fp = stdin;
-  if (argc > POS_NOMBRE_ARCHIVO)
-  {
-    fp = fopen(argv[POS_NOMBRE_ARCHIVO], "r");
-    if ( fp == NULL ) return ARCHIVO_NO_ENCONTRADO;
-  }
-  while ( !feof(fp) )
-  {
-    int c = fgetc(fp);
-    if ( c != EOF )
-      printf("%c", (char) c);
-  }
-  if (argc > POS_NOMBRE_ARCHIVO)
-    fclose(fp);
-  return SALIDA_NORMAL;
+ FILE *fp = stdin;
+ if (argc > POS_NOMBRE_ARCHIVO)
+ {
+   fp = fopen(argv[POS_NOMBRE_ARCHIVO], "r");
+   if ( fp == NULL ) return ARCHIVO_NO_ENCONTRADO;
+ }
+ while ( !feof(fp) )
+ {
+   int c = fgetc(fp);
+   if ( c != EOF )
+     printf("%c", (char) c);
+ }
+ if (argc > POS_NOMBRE_ARCHIVO)
+   fclose(fp);
+ return SALIDA_NORMAL;
 }
